a58a81b1f16445cdee721e7fccb2a69a3c943a24
mayabargig <mayabargig@gmail.com>
2023-12-10 11:59:39 +0200

    react app done!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
-import { useState } from "react";
+import { useEffect, useContext} from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 function App () {
-    const [books, setBooks] = useState([]);
+    const {fetchBooks} = useContext(BooksContext);
 
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if(book.id === id){
-                return {...book, title: newTitle};
-            }
+    useEffect(() => {
+        fetchBooks();
+    }, [fetchBooks]);
 
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    };
-
-    const deleteBookById = (id) => {
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-        setBooks(updatedBooks);
-    };
-
-    const createBook = (title) => {
-        const updatedBooks = [ ...books, { 
-            id: Math.round(Math.random() * 9999), 
-            title } ];
-        setBooks(updatedBooks);
-    };
-    
     return(
      <div className="app">
-        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById}/>
-        <BookCreate onCreate={createBook}/>
+        <h1>Reading List</h1>
+        <BookList />
+        <BookCreate />
     </div>
     );
 }
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
 import BookEdit from './BookEdit';
 
-function BookShow ({book, onDelete, onEdit}){
+function BookShow ({book }){
     const [showEdit, setShowEdit] = useState(false);
+    const { deleteBookById } = useContext(BooksContext);
 
     const handelDeleteClick = () => {
-        onDelete(book.id);
+        deleteBookById(book.id);
     };
 
     const handelEditClick = () => {
         setShowEdit(!showEdit);
     };
 
-    const handelSubmit = (id, newTitle) => {
+    const handelSubmit = () => {
         setShowEdit(false);
-        onEdit(id, newTitle)
     };
 
     let content = <h3>{book.title}</h3>
@@ -24,6 +25,10 @@ function BookShow ({book, onDelete, onEdit}){
 
     return (
     <div className="book-show">
+        <img 
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/300/200`}
+        />
         <div>{content}</div>
         <div className="actions">
             <button className="edit" onClick={handelEditClick}>
